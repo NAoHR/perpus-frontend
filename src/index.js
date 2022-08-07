8c4b59ff8ee007260b30437586c6f5f8e4957e46
naohr <najmim625@gmail.com>
2022-08-07 21:08:51 +0700

    update: implement browserrouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import './styles/globals.css';
+import About from './pages/About';
+import Books from './pages/Books';
+import Guide from './pages/Guide';
+import Home from './pages/Home';
+import Book from './pages/Book';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path="/books" element={<Books />} />
+          <Route path="/books/:bookId" element={<Book />} />
+          <Route path='guide' element={<Guide />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
