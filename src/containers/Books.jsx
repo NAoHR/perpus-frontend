9e347d6fc9da18c0d797f98a5681d398af0969b7
naohr <najmim625@gmail.com>
2022-08-12 23:12:51 +0700

    add: book page init

--- src/containers/Books.jsx
+++ src/containers/Books.jsx
@@ -1,9 +1,30 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
+import "../styles/containers/Books.css";
 export default function Books(){
     return (
         <>
         <Navbar />
-        books
+        <section className="book">
+            <div className="s-size card-wrap">
+                <div className="search">
+                    <div className="input-box round-it">
+                        <input type="text" name="filter" className="poppins b-white c-orange bold" autoCorrect="false" placeholder="Cari disini"/>
+                        
+                    </div>
+                    <div className="filter-box round-it">
+                    <select name="cars" id="cars" form="carform" className="poppins c-white bold b-orange">
+                        <option value="all">All</option>
+                        <option value="life">life</option>
+                        <option value="science">science</option>
+                        <option value="wew">wew</option>
+                    </select>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className="books s-size flex">
+
+        </section>
         </>
     )
 }
